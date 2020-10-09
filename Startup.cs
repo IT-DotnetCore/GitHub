d92cac2b2ece24bc7a05cae4988126c4a40c8cd7
hasanovkamol <71973508+hasanovkamol@users.noreply.github.com>
2020-10-09 18:17:37 +0500

    add new static project

--- Startup.cs
+++ Startup.cs
@@ -20,13 +20,10 @@ namespace TestLibrary
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,7 +33,6 @@ namespace TestLibrary
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();

f6c887428acc8a623a1aaed8a29e9d0dd830295f
hasanovkamol <71973508+hasanovkamol@users.noreply.github.com>
2020-10-10 13:12:15 +0500

    sdadad

---
+++ Migrations/20201010075700_dbcreate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TestLibrary.Migrations
+{
+    public partial class dbcreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

96ebdfaabde552999b32a7d43df54cacd20cbd44
hasanovkamol <71973508+hasanovkamol@users.noreply.github.com>
2020-10-13 19:01:46 +0500

    add new class for select  language

---
+++ Canstanta/Language.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Identity.UI.V3.Pages.Account.Manage.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestLibrary.Canstanta
+{
+    public class Language
+    {
+        private List<string> items = new List<string>();
+       
+     
+           
+    }
+}

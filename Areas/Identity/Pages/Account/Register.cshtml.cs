fe48d97f40fd81c04c009f4e71aa2e255553f7ea
Dell <Dell@DESKTOP-77AKTOJ>
2020-10-13 19:31:40 +0500

    changes

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -16,7 +16,7 @@ using Microsoft.Extensions.Logging;
 
 namespace TestLibrary.Areas.Identity.Pages.Account
 {
-    [Authorize(Roles ="Admin")]
+    //[Authorize(Roles ="Admin")]
     public class RegisterModel : PageModel
     {
         private readonly SignInManager<IdentityUser> _signInManager;
